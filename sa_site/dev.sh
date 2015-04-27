2539147922365b15fbf45293d5bf4b505aa0fa1e
Tyler Romeo <tylerromeo@gmail.com>
2015-04-27 17:39:51 -0400

    Handle upgrade to Debian Jessie and PHP 5.5
    Cleaned up Dockerfile and Makefile, moving Composer installation to the
    Makefile so that it does not need to be installed into the Docker
    image. Also removed the SSH key, as it is not really necessary.
    
    Finished adjustment of packages for Debian Jessie, switching from
    mod_fastcgi to mod_proxy_fcgi, and replacing the PHP5-ElastiCache
    extension with a version compiled for PHP 5.5.
    
    Finally, added the dotdeb repository to allow for downgrading PHP to 5.5
    rather than the 5.6 default that comes with Jessie.

--- sa_site/dev.sh
+++ sa_site/dev.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-set -u
-set -e
-set -o pipefail
-
-# Re-establish the symbolic link to composer's vendor directory
-if [ ! -e /vagrant/sa_site_v2/website/application/third_party/vendor ]; then
-    /usr/bin/docker exec sportarc-sa_site \
-		    ln -sf /opt/third_party /opt/sa_site/website/application/third_party/vendor
-fi
+set -euo -o pipefail
 
 # Re-copy over configuration files
 if [ ! -f /vagrant/sa_site_v2/website/application/config/sessions.php ]; then
