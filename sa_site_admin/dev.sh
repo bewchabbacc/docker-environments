415036881db7711eff428101f768ac20e2c4fc7c
koxon <nmenciere@sportarchive.tv>
2015-05-28 00:15:08 -0400

    Structure updates

--- sa_site_admin/dev.sh
+++ sa_site_admin/dev.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-set -u
-set -e
-set -o pipefail
+set -euo -o pipefail
 
 # Re-copy over configuration files
-if [ ! -f /vagrant/sa_site_admin/application/config/orchestrate.php ]; then
-    cp /vagrant/config/* /vagrant/sa_site_admin/application/config/
+if [ ! -f /vagrant/sa_site_admin/website/application/config/sessions.php ]; then
+    cp /vagrant/config/* /vagrant/sa_site_admin/website/application/config/
 fi
+
+cp /vagrant/config/sessions_dev.php /vagrant/sa_site_admin/website/application/config/sessions.php
+
